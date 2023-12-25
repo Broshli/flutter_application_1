0b7bca5bb1c15fb242832764b5c6412c3d11dcc9
Ankit Saklani <97657457+Broshli@users.noreply.github.com>
2023-12-25 19:01:01 +0530

    summary

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,17 +3,23 @@ import 'package:flutter_application_1/view/view.dart';
 
 // Statelss Widget
 class MyApp extends StatelessWidget {
-  const MyApp({
+  final name = 'Flutter App';
+
+   const MyApp({
     super.key,
+    // required this.name,
   });
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter App",
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ViewPage(),
+      home: const ViewPage(
+
+      ),
     );
   }
 }
